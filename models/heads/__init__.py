fb6f941287a26668b0e66f980afdc9d36c011465
Kershockr <tim.merino@live.com>
2022-12-19 23:12:17 -0500

    Added new Leaky ReLU head to init file

--- models/heads/__init__.py
+++ models/heads/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .topdown_heatmap_base_head import TopdownHeatmapBaseHead
 from .topdown_heatmap_simple_head import TopdownHeatmapSimpleHead
+from .topdown_heatmap_simple_head_LReLU import TopdownHeatmapSimpleHeadLReLU
 
 __all__ = [
     'TopdownHeatmapSimpleHead', 
-    'TopdownHeatmapBaseHead'
+    'TopdownHeatmapBaseHead',
+    'TopdownHeatmapSimpleHeadLReLU'
 ]

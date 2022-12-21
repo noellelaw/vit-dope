0fc362de3794c20e5dab5f59336b0192281dd202
Kershockr <tim.merino@live.com>
2022-12-20 20:07:46 -0500

    first pass at loss on every layer

--- models/heads/__init__.py
+++ models/heads/__init__.py
@@ -2,9 +2,11 @@
 from .topdown_heatmap_base_head import TopdownHeatmapBaseHead
 from .topdown_heatmap_simple_head import TopdownHeatmapSimpleHead
 from .topdown_heatmap_simple_head_LReLU import TopdownHeatmapSimpleHeadLReLU
+from .topdown_heatmap_simple_head_layerloss import TopdownHeatmapSimpleHeadLayerLoss
 
 __all__ = [
     'TopdownHeatmapSimpleHead', 
     'TopdownHeatmapBaseHead',
-    'TopdownHeatmapSimpleHeadLReLU'
+    'TopdownHeatmapSimpleHeadLReLU',
+    'TopdownHeatmapSimpleHeadLayerLoss'
 ]
